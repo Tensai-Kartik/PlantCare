fbd7df3019d988a62cd284a63f735fda5ef770ad
Kartik <tensai.kartik@gmail.com>
2026-09-06 13:59:36 +0530

    Update Multi-Model consensus toggle switch colors (green when on, black when off)

--- frontend/src/pages/AnalyzePlant.tsx
+++ frontend/src/pages/AnalyzePlant.tsx
@@ -124,7 +124,7 @@ export const AnalyzePlant: React.FC<AnalyzePlantProps> = ({
                 onChange={(e) => setEnableModelComparison(e.target.checked)}
                 className="sr-only peer"
               />
-              <div className="w-11 h-6 bg-surface-elevated peer-focus:outline-hidden rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-emerald-600"></div>
+              <div className="w-11 h-6 bg-black dark:bg-black border border-zinc-800 dark:border-zinc-700 peer-focus:outline-hidden rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-emerald-600 dark:peer-checked:bg-emerald-500 peer-checked:border-emerald-600 dark:peer-checked:border-emerald-500"></div>
             </label>
           </div>
 
